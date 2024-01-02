d878c539dca6375ec432c47d7ec9627d01118f1b
Ben Morse <ben.morse@torchbox.com>
2024-01-02 10:39:27 +0000

    init

---
+++ wagtail_shortcode/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WagtailShortcodeAppConfig(AppConfig):
+    label = "wagtail_shortcode"
+    name = "wagtail_shortcode"
+    verbose_name = "Wagtail shortcode"
---
+++ wagtail_shortcode/test/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WagtailShortcodeTestAppConfig(AppConfig):
+    label = "wagtail_shortcode_test"
+    name = "wagtail_shortcode.test"
+    verbose_name = "Wagtail shortcode tests"
